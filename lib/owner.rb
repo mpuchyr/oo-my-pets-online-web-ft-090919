27fc91dd5e8c5bf75552fadb598d5fbbbc8c26f1
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-18 22:12:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,13 @@
 require 'pry'
 class Owner
-  attr_reader :name
+  attr_reader :name, :species
   
   @@all = []
   
   
   def initialize(name)
     @name = name
+    @species = "human"
     @@all << self
   end
   
