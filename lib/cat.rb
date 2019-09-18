aa8d50774b2605a5203cc2e2e160dbb488e9cad8
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-18 22:19:41 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initalize(name)
+    @name = name
+    @owner = Owner.new
+    @@all << self
+  end
+  
 end
